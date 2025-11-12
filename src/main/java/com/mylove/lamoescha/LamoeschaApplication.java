208815007349029ee8909f77662eb49ebd5512de
ahmad <ahmad.alhennawi@uni-ulm.de>
2025-11-12 11:28:40 +0100

    init

---
+++ src/main/java/com/mylove/lamoescha/LamoeschaApplication.java
@@ -0,0 +1,13 @@
+package com.mylove.lamoescha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamoeschaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamoeschaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mylove/lamoescha/LamoeschaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylove.lamoescha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamoeschaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
